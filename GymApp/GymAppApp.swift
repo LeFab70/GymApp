41e4cf49b50f001e6bdcf3b9f77df5f715bff512
LeFab70 <kouonang2002@gmail.com>
2025-08-16 09:25:30 -0300

    Initial Commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Fabrice Kouonang on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
